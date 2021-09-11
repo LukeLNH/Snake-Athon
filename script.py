d4f89e81aa959816f361f1a8f0bd86df97d76f57
Lukea <hl2001@students.cs.ubc.ca>
2021-09-11 16:41:49 -0700

    initial snake moving functionality

--- script.py
+++ script.py
@@ -1,53 +1,100 @@
 import pygame
-# testing some stuff
+import random
+
+screen_width = 640
+screen_height = 640
+grid_size = 30
 
 class Snake():
 
     def __init__(self):
-        self.snake_blocks = [] # idx 0 is the head, the rest is the body
-        # set initial length of snake
-        pass
+        self.snake_blocks = [(screen_width/2, screen_height/2), (screen_width/2 - 1*grid_size, screen_height/2), (screen_width/2 - 2*grid_size, screen_height/2)] # idx 0 is the head, the rest is the body
+        self.last_tail = (0,0)
+        self.score = 0
+        self.direction = (1,0)
+        self.alive = True
+
+    def check_dead(self, new_head):
+        return new_head[0] < 0 or new_head[1] < 0 or new_head[0] > screen_width - grid_size or new_head[1] > screen_height - grid_size or new_head in self.snake_blocks[1:]
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            direction = self.direction
+            if event.type == pygame.KEYDOWN:
+                if event.type == pygame.K_UP:
+                    direction = (0, -1)
+                elif event.type == pygame.K_DOWN:
+                    direction = (0, 1)
+                elif event.type == pygame.K_LEFT:
+                    direction = (-1, 0)
+                elif event.type == pygame.K_RIGHT:
+                    direction = (1, 0)
+        
+        # check not moving backwards or some shit
+        if direction[0] == -self.direction[0] or direction[1] == -self.direction[1]:
+            pass
+        else:
+            # add head block
+            current_head = self.snake_blocks[0]
+            new_head = (current_head[0] + direction[0] * grid_size, current_head[1] + direction[1] * grid_size)
+            self.snake_blocks.insert(0, new_head)
+            self.last_tail = self.snake_blocks.pop(-1)
+            # change direction
+            self.direction = direction
 
-    def eat_food(self):
-        # has to grow after eating
-        pass
+        # check dead (run into own body or go out of screen)
+        if self.check_dead(new_head):
+            self.alive = False
 
-    def check_dead(self):
-        pass
+
+    def eat_food(self, food):
+        self.score = self.score + food.score
 
     def get_score(self):
-        return len(self.snake_blocks)
+        return self.score
 
 
 class Food():
-
-    def __init__(self):
-        self.position = (0,0)
-        self.score = 100
+    def __init__(self, position = (0,0)):
+        self.position = position
+        self.score = 1
         self.check_eaten = False
-        pass
 
+def render_food(food: Food): # Implement after we figure out pygame
+    pass
 
 def make_food():
-    pass
+    position = (random.randint(0,640), random.randint(0,640))
+    food = Food(position)
+    render_food(food)
 
 def display_score():
     pass
 
 def setup():
     # Gonna need a shit ton of helper functions :)
-    pass
+    # pygame.init()
+    # clock = pygame.time.Clock()
+    # screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
+    # surface = pygame.Surface(screen.get_size())
+    # surface = surface.convert()
 
-def start_game():
-    # where the main while loop will be
+    # for x in range(screen_height):
+    #     for y in range(screen_width):
+    #         rect = pygame.Rect((x*grid_size, y*grid_size), (grid_size, grid_size))
+    #         pygame.draw.rect(surface, (0,0,255), rect)
+    
+    # while True:
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             break
+    #     clock.tick(10)
+    #     snake.move()
+    #     if eating food, increase score and leave tail alone, else remove tail
     pass
 
 def main():
     setup()
-    start_game()
 
 if __name__ == '__main__':
     main()
